45ecbfbe916be75c594f06f78d3e340d1ac17e32
Pascal Rainville <pascal.rainville@gmail.com>
2024-04-13 20:11:50 -0400

    .

--- src/build.js
+++ src/build.js
@@ -2,11 +2,13 @@ const getRecipes = require("./getRecipes.js");
 const showRecipe = require("./showRecipe.js");
 const execSh = require("@jome/core/execSh");
 const path = require("path");
-const { build } = require("@jome/core");
+const { build, writeSync } = require("@jome/core");
 module.exports = (f) => {
   let force = f;
 
   execSh("rm -R ./docs/assets");
+
+  execSh("rm -R ./docs/r/*");
 
   execSh("cp -R ./public ./docs/assets");
 
@@ -25,4 +27,13 @@ module.exports = (f) => {
   );
 
   let recipes = getRecipes();
+
+  recipes.forEach(function (recipe) {
+    let html = showRecipe(recipe);
+    let id = recipe.id || recipe["$id"];
+    if (id) {
+      execSh(`mkdir ./docs/r/${id}`);
+      writeSync(html, { to: `./docs/r/${id}/index.html` });
+    }
+  });
 };
